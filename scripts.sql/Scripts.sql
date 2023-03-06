31c31e4b4fec9f2047479097664061f4f75096f7
RodrigodeOliveiraPeixer <109769171+RodrigodeOliveiraPeixer@users.noreply.github.com>
2023-03-06 15:13:58 -0300

    Update Scripts.sql

--- scripts.sql/Scripts.sql
+++ scripts.sql/Scripts.sql
@@ -118,6 +118,24 @@ Passo 19:
 alter table cursos
 add column idcurso int first;
 
+Passo 20:
+alter table cursos
+add primary key (idcurso)
+
+Passo 21:
+
+insert into cursos values
+('1','HTML4','Curso de HTML5','40','37','2014'),
+('2','Algoritmos','Lógica de programação','20','15','2014'),
+('3','Photoshop','Dicas de Photoshop CC','10','8','2014'),
+('4','PGP','Curso de PHP para iniciantes','40','20','2010'),
+('5','Jarva','Introdução à Linguagem JAVA','10','29','2000'),
+('6','MySQL','Banco de dados MySQL','30','15','2016'),
+('7','Word','Curso completo de Word','40','30','2016'),
+('8','Sapateado','Danças Rítmicas','40','30','2018'),
+('9','Cozinha Árabe','Aprenda a fazer Kibe','40','30','2018'),
+('10','Youtuber','Gerar polêmicas e ganhar inscritos','5','2','2018');
+
 
 
 
